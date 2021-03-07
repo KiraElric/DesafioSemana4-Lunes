dacdb2c5368df4fde6ca1807617449e1500ae368
KiraElric <lissette.urso@gmail.com>
2021-03-07 01:46:08 -0300

    Adicionando el modelo y las conecciones con la base de datos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
   def index
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def dashboard
   end
 
   def create
+    @post = Post.create(title: params[:title], image_url: params[:image_url], content: params[:content])
   end
 end
