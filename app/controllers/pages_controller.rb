c7b4785d87f1f86f93ce2809db89b77cda76c606
KiraElric <lissette.urso@gmail.com>
2021-03-07 02:47:48 -0300

    Creación metodo spoiler, mejorando las vistas y adicionando registros en la base de datos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only: :dashboard
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   validates :title, :image_url, :content, presence: true
+
+  before_save :spoiler
+  def spoiler
+    self.content = (self.content.downcase).gsub("spoiler","")
+  end
 end
